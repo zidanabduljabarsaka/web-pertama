7823cc0f2e6c4f575e1e9fff294bf37b7b793757
Zidan Abdul Jabar <zidanabduljabarsaka@users.noreply.github.com>
2026-04-25 10:38:56 +0700

    Initial commit with README and ignore

--- drizzle/0000_peaceful_siren.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `users` (
-	`id` serial AUTO_INCREMENT NOT NULL,
-	`name` varchar(255) NOT NULL,
-	`email` varchar(255) NOT NULL,
-	`created_at` timestamp DEFAULT (now()),
-	CONSTRAINT `users_id` PRIMARY KEY(`id`),
-	CONSTRAINT `users_email_unique` UNIQUE(`email`)
-);
--- src/db/schema.ts
+++
@@ -1,8 +0,0 @@
-import { mysqlTable, serial, varchar, text, timestamp } from "drizzle-orm/mysql-core";
-
-export const users = mysqlTable("users", {
-  id: serial("id").primaryKey(),
-  name: varchar("name", { length: 255 }).notNull(),
-  email: varchar("email", { length: 255 }).notNull().unique(),
-  createdAt: timestamp("created_at").defaultNow(),
-});
